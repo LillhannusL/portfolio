d2fb49252e072a9ea88a3e419989d6d5304bbb31
Lisa-Ni <niklasson.lisa@gmail.com>
2025-09-24 15:32:35 +0200

    start on a navbar and darkmodetoggle function

--- src/components/darkmodetoggle/DarkModeToggle.jsx
+++ src/components/darkmodetoggle/DarkModeToggle.jsx
@@ -1,7 +1,21 @@
 import React from 'react';
+import './darkmodetoggle.css';
+import { useState, useEffect } from 'react';
 
 function DarkModeToggle() {
-	return <div>DarkModeToggle</div>;
-}
+	const [dark, setDark] = useState(false);
 
+	useEffect(() => {
+		if (dark) {
+			document.body.classList.add('dark-mode');
+		} else {
+			document.body.classList.remove('dark-mode');
+		}
+	}, [dark]);
+	return (
+		<button className="darkmodetoggle-button" onClick={() => setDark(!dark)}>
+			{!dark ? '🌙' : '☀️'}
+		</button>
+	);
+}
 export default DarkModeToggle;
