947a68561fa2d7e65be9bbfd5f7817c7a97805cf
Lisa-Ni <niklasson.lisa@gmail.com>
2025-09-25 12:29:45 +0200

    navbar

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -1,7 +1,14 @@
 import React from 'react';
+import './footer.css';
 
 function Footer() {
-	return <div>Footer</div>;
+	return (
+		<div className="footer">
+			<h1>L.Lillhannus</h1>
+			<p>LinkedIn</p>
+			<p>Github</p>
+		</div>
+	);
 }
 
 export default Footer;
--- src/sections/contact/Contact.jsx
+++ src/sections/contact/Contact.jsx
@@ -1,7 +1,18 @@
 import React from 'react';
+import './contact.css';
 
 function Contact() {
-	return <div>Contact</div>;
+	return (
+		<div className="contact">
+			<h1 id="contact">Contact</h1>
+			<p>
+				Lorem ipsum dolor sit amet consectetur adipisicing elit. Officia
+				distinctio eos laudantium id quos dignissimos hic at quisquam nihil!
+				Esse accusantium nemo, asperiores consequatur ratione rem sapiente
+				recusandae voluptatibus quasi!
+			</p>
+		</div>
+	);
 }
 
 export default Contact;
